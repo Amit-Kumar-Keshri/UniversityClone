6d1db11231d7e16c2eb0f0e19ac353c1cd1c6a46
Virus <amitkeshri003@gmail.com>
2022-11-23 16:33:09 +0530

    added media query

---
+++ index.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function () {
+
+  mobile_menu_init();
+});
+
+jQuery(document).on("click", ".ham_burg_menu", function () {
+  jQuery(".menu").slideToggle();
+});
+
+jQuery(window).resize(function () {
+  mobile_menu_init();
+});
+
+function mobile_menu_init() {
+  if (window.matchMedia("(max-width: 600px)").matches) {
+    jQuery(".menu").hide();
+    
+      if(!jQuery('.ham_burg_menu').length){
+        jQuery(".menu").before('<button class="ham_burg_menu"><img src="img/hamburger_opened.png"/></button>');
+      }
+      
+  } else {
+    jQuery(".menu").show();
+    jQuery(".ham_burg_menu").remove();
+  }
+}
